c539fee247f9c6613f4e2172a1bde7000bcbcf7b
eichingertim <business.timeichinger@gmail.com>
2019-09-26 11:08:06 +0200

    added book-list widget

--- book_management_app/lib/books_widget.dart
+++ book_management_app/lib/books_widget.dart
@@ -3,9 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class BooksPage extends StatelessWidget {
+
+  final List<String> entries = <String>['Book 1', 'Book 2', 'Book 3'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            child; Text('Entry ${entries[index]}'),
+          );
+        }
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: Colors.blue,
