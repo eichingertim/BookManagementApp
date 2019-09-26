d4166c65e7518d31063a86c40d373d8166d4a73b
eichingertim <business.timeichinger@gmail.com>
2019-09-26 16:22:16 +0200

    added list

---
+++ book_management_app/lib/book_item.dart
@@ -0,0 +1,13 @@
+class BookItem {
+
+  String name;
+  int currentPage;
+  int timeRead;
+
+  BookItem(String name, int currentPage, int timeRead) {
+    this.name = name;
+    this.currentPage = currentPage;
+    this.timeRead = timeRead;
+  }
+
+}
--- book_management_app/lib/books_widget.dart
+++ book_management_app/lib/books_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sqflite/sqflite.dart';
 
 class BooksPage extends StatelessWidget {
 
@@ -10,12 +11,17 @@ class BooksPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(16),
         itemCount: entries.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            child; Text('Entry ${entries[index]}'),
+          return GestureDetector(
+            child: Container(
+              child: Text('Entry ${entries[index]}'),
+              height: 50,
+              ),
+            onTap: () => Scaffold
+              .of(context)
+              .showSnackBar(SnackBar(content: Text(entries[index]))),
           );
         }
       ),
